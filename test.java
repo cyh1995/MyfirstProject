bb177ed2d6e28a708ea909a76f79b1b82369462d
cyh1995 <969963179@qq.com>
2020-07-10 17:54:24 +0800

    fhyxh change test.java

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test{
     public static void main(String []args){
         System.out.println(Add(1,2));
+        System.out.println("this is fhyxh here");
     }
     public static int Add(int a, int b){
         return a+b;
